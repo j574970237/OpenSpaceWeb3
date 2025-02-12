01bc3b2b80eda46e41ba185ef31f816fef99b7e5
JackZhu <574970237@qq.com>
2025-02-12 10:26:39 +0800

    fix:优化代码，抽象出公共方法

--- W1/D1/pow.go
+++ W1/D1/pow.go
@@ -14,43 +14,37 @@ import (
 1. 直到满足 4 个 0 开头的哈希值，打印出花费的时间、Hash 的内容及Hash值。
 2. 再次运算直到满足 5 个 0 开头的哈希值，打印出花费的时间、Hash 的内容及Hash值。
 **/
+
 func main() {
     nickname := "JackZhu"
-    nonce := 0
-    targetPrefix4 := "0000"
 
     // 寻找满足4个0开头的哈希值
+    targetPrefix4 := "0000"
+    hashStr, nonce, spendTime := findHashWithPrefix(nickname, targetPrefix4)
+    fmt.Printf("寻找满足4个0开头的哈希值:\n")
+    fmt.Printf("花费的时间: %s\n", spendTime)
+    fmt.Printf("Hash的内容: %s%d\n", nickname, nonce)
+    fmt.Printf("Hash值: %s\n", hashStr)
+
+    // 寻找满足5个0开头的哈希值
+    targetPrefix5 := "00000"
+    hashStr, nonce, spendTime = findHashWithPrefix(nickname, targetPrefix5)
+    fmt.Printf("寻找满足5个0开头的哈希值:\n")
+    fmt.Printf("花费的时间: %s\n", spendTime)
+    fmt.Printf("Hash的内容: %s%d\n", nickname, nonce)
+    fmt.Printf("Hash值: %s\n", hashStr)
+}
+
+// findHashWithPrefix 寻找满足指定前缀的哈希值
+func findHashWithPrefix(nickname string, targetPrefix string) (string, int, time.Duration) {
+    nonce := 0
     startTime := time.Now()
     for {
         hash := sha256.Sum256([]byte(fmt.Sprintf("%s%d", nickname, nonce)))
         hashStr := hex.EncodeToString(hash[:])
-        if strings.HasPrefix(hashStr, targetPrefix4) {
+        if strings.HasPrefix(hashStr, targetPrefix) {
             spendTime := time.Since(startTime)
-            fmt.Printf("寻找满足4个0开头的哈希值:\n")
-            fmt.Printf("花费的时间: %s\n", spendTime)
-            fmt.Printf("Hash的内容: %s%d\n", nickname, nonce)
-            fmt.Printf("Hash值: %s\n", hashStr)
-            break
-        }
-        nonce++
-    }
-
-    // 重置随机数和目标前缀
-    nonce = 0
-	targetPrefix5 := "00000"
-
-    // 寻找满足5个0开头的哈希值
-	startTime = time.Now()
-    for {
-        hash := sha256.Sum256([]byte(fmt.Sprintf("%s%d", nickname, nonce)))
-        hashStr := hex.EncodeToString(hash[:])
-        if strings.HasPrefix(hashStr, targetPrefix5) {
-            spendTime := time.Since(startTime)
-            fmt.Printf("寻找满足5个0开头的哈希值:\n")
-            fmt.Printf("花费的时间: %s\n", spendTime)
-            fmt.Printf("Hash的内容: %s%d\n", nickname, nonce)
-            fmt.Printf("Hash值: %s\n", hashStr)
-            break
+            return hashStr, nonce, spendTime
         }
         nonce++
     }
