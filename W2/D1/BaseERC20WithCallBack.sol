3907da758344c815baa457479d46318c96e7b756
JackZhu <574970237@qq.com>
2025-02-18 13:08:20 +0800

    feat: contract NFTMarket first commit.

--- W2/D1/BaseERC20WithCallBack.sol
+++ W2/D1/BaseERC20WithCallBack.sol
@@ -13,8 +13,8 @@ contract BaseERC20WithCallBack is ERC20 {
         _mint(msg.sender, 100000000 * 10 ** 18);
     }
     
-    function transferWithCallback(address _to, uint256 amount) public returns (bool) {
-        _transfer(msg.sender, _to, amount);
+    function transferWithCallback(address _from, address _to, uint256 amount) public returns (bool) {
+        _transfer(_from, _to, amount);
         // 在转账时，如果目标地址是合约地址，调用目标地址的 tokensReceived 方法
         if (isContract(_to)) {
             // 调用目标地址的 tokensReceived 方法
